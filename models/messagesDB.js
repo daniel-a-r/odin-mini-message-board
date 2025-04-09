22cf2624e167cbc05c5b863bfc447aadd59a2a23
daniel-a-r <daniel.a-r@outlook.com>
2025-04-09 05:19:22 -0400

    initialize message list to be empty

--- models/messagesDB.js
+++ models/messagesDB.js
@@ -1,17 +1,4 @@
-export const messages = [
-  {
-    user: 'Amando',
-    content: 'Hi there!',
-    timestamp: new Date(),
-    id: '826a6ba9-24a5-46ac-8aec-eda7f3c9bb59',
-  },
-  {
-    user: 'Charles',
-    content: 'Hello World!',
-    timestamp: new Date(),
-    id: 'ee3a9de8-d811-4105-b289-85c2b71f6111',
-  },
-];
+export const messages = [];
 
 export const getMessageById = (messageId) => {
   return messages.find((message) => message.id === messageId);
