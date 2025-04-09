f06b236cde212b2d9503148f578359b9627850d4
daniel-a-r <daniel.a-r@outlook.com>
2025-04-09 05:16:58 -0400

    delete extra line

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import indexRouter from './routes/indexRouter.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.set('views', path.join(import.meta.dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(import.meta.dirname, 'public')));
